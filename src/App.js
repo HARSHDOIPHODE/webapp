5bb686030a6d418bd5de231f6f5fa62ad480abac
harshdoiphode <harshdoiphode1308@gmail.com>
2022-02-01 14:27:44 +0530

    harsh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-   <div><h1>or nalle</h1>
-   <h1> teri ****** </h1>
-   <h1> chal ja ghina</h1>
+   <div className="app">
+     hello
      </div>
   )
 

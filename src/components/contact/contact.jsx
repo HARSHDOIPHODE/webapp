fa4e3409b4e055c69955adde9df5e8d63fe3311e
harshdoiphode <harshdoiphode1308@gmail.com>
2022-02-03 12:30:53 +0530

    harsh

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -9,28 +9,9 @@ export default function Contact() {
     setMessage(true);
   };
   return (
-<<<<<<< HEAD
   <div className="contact">
     harsh
 
   </div>
   )
 }
-=======
-    <div className="contact" id="contact">
-      <div className="left">
-        <img src="assets/shake.svg" alt="" />
-      </div>
-      <div className="right">
-        <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
-        </form>
-      </div>
-    </div>
-  );
-}
->>>>>>> a0da2c5f83b7c05d9181373eeaf02bf66f056e26
